10f2bca64433d43f87359af237f75c0d376ef9b2
Niklas Molnes Hole <nikasmh@hotmail.com>
2021-04-15 01:21:49 +0200

    Make it possible to relocate with same image

--- app.py
+++ app.py
@@ -28,17 +28,27 @@ def handle_move(data):
     print('move: ' + data)
 
 
-@socketio.on('locate')
-def handle_locate():
-    print('locate')
+def get_position(new_image=True):
     position = find_robot_position.locate(
         assumed_position=[0, 0],
-        capture_new_image=True
+        capture_new_image=new_image
     )
     if position:
         emit("position", str(position[0]) + "," + str(position[1]))
     else:
         emit("position-failed", "Could not find position")
+
+
+@socketio.on('locate')
+def handle_locate():
+    print('locate')
+    get_position(new_image=True)
+
+
+@socketio.on('relocate')
+def handle_locate():
+    print('relocate')
+    get_position(new_image=False)
 
 
 if __name__ == '__main__':
