3147901103921a2816d9ec71526af79d9a47c939
Niklas Molnes Hole <nikasmh@hotmail.com>
2021-04-14 23:42:32 +0200

    Expose web server over network

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 -m flask run
+python3 -m flask run --host=0.0.0.0
--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 # Make sure the raspberry pi has everything set up correctly
 pip3 install -r requirements.txt
+
+sudo apt install ufw
+sudo ufw enable
+sudo ufw allow 5000
